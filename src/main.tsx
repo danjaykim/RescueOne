9d37c958e0d7bf412f924f5ecda2b8f65a2a6152
Daniel Kim <danjkim831@gmail.com>
2025-02-07 14:18:39 -0800

    Adjusted main setup with createBrowserRouter/RouterProvider, prettier as default, and removed unnecessary files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+])
+
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Root element was not found')
+
+console.table(import.meta.env)
+
+const root = createRoot(rootElement)
+root.render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 )
